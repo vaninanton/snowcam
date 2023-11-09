701164c115dbacfea1e65f897eafb2a7fb9119d3
Tony V <vaninanton@gmail.com>
2023-11-10 01:42:17 +0600

    loading animation

--- src/main.js
+++ src/main.js
@@ -5,4 +5,4 @@ import iosPWASplash from "ios-pwa-splash/index.js"
 
 createApp(App).mount("#app");
 
-iosPWASplash('android-chrome-512x512.png', 'white');
+iosPWASplash('android-chrome-512x512.png', '#0f172a');
