be5e8ebcd2eb2ea8640aa7d5ce27d5a5294cb914
Tony V <vaninanton@gmail.com>
2023-11-10 01:35:01 +0600

    added ios pwa splash

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import iosPWASplash from "ios-pwa-splash/index.js"
 
 createApp(App).mount("#app");
+
+iosPWASplash('android-chrome-512x512.png', 'white');
