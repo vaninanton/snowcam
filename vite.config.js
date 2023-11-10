a416fdf95346c800203a6e3cfc9a167722112bda
Tony V <vaninanton@gmail.com>
2023-11-10 15:50:45 +0600

    removed base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/snowcam/",
   server: { https: true },
   plugins: [vue(), splitVendorChunkPlugin(), mkcert()],
 });
