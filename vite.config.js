27ccb09039e068f8f845c5d61157732723a3f961
Tony V <vaninanton@gmail.com>
2024-05-01 15:59:14 +0500

    npm update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { splitVendorChunkPlugin } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
-  plugins: [vue(), splitVendorChunkPlugin(), mkcert()],
+  plugins: [vue(), mkcert()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
