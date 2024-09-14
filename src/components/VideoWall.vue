7b14c3c33be9bf0893d826aa4e426eb655baa4c2
Tony V <vaninanton@gmail.com>
2024-09-14 08:15:07 +0500

    added videowall

---
+++ src/components/VideoWall.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+const VideoItem = defineAsyncComponent(() => import("./VideoWallItem.vue"));
+
+const videos = [
+  {
+    title: "Ледник Богдановича",
+    description: "Верхняя станция",
+    src: "https://ipcam.kz/cam6/index.m3u8",
+    poster: "/previews/3212b47c65ccc0cb9759f5c08632c563.jpg",
+  },
+  {
+    title: "Средняя станция",
+    description: "Средняя станция",
+    src: "https://ipcam.kz/cam5/index.m3u8",
+    poster: "/previews/ffa10e45b1d87ab0aa8796bc7969046b.jpg",
+  },
+  {
+    title: "Бугель",
+    description: "Базовая станция",
+    src: "https://ipcam.kz/cam2/index.m3u8",
+    poster: "/previews/d5361ff25a96271df9ac614d9138806a.jpg",
+  },
+  {
+    title: "Подъемник",
+    description: "Базовая станция",
+    src: "https://ipcam.kz/cam1/index.m3u8",
+    poster: "/previews/197f1dde9a2c929cca5496f225cc2d62.jpg",
+  },
+];
+</script>
+<template>
+  <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-1 mx-auto">
+    <li
+      class="group relative rounded-3xl bg-slate-50 p-6 lg:p-0 dark:bg-slate-800/80 dark:highlight-white/5 hover:bg-slate-100 dark:hover:bg-slate-700/50"
+      v-for="video in videos"
+      v-bind:key="video.title"
+    >
+      <VideoItem :video="video" />
+    </li>
+  </ul>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,21 @@ export default defineConfig({
   server: { https: true },
   plugins: [vue(), mkcert()],
   build: {
+    chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes("node_modules")) {
+            if (id.includes("/vue/") || id.includes("/@vue/")) {
+              return "vue";
+            }
+
             return id
               .toString()
               .split("node_modules/")[1]
               .split("/")[0]
-              .toString();
+              .toString()
+              .toLowerCase();
           }
         },
       },
